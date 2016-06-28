e66e48c68d8445c404ce9ec2a0f126dcd03ac326
jblang94 <jbarlang@gmail.com>
2016-06-28 05:10:06 +0000

    Fixed implementation to match gist

--- contact.rb
+++ contact.rb
@@ -1,14 +1,22 @@
-# Represent a Contact in the ContactList
+require 'csv'
+require 'pry-byebug'
+# Represents a person in an address book.
+# The ContactList class will work with Contact objects instead of interacting with the CSV file directly
 class Contact
 
   @@contacts_count = 1
+  @@contacts = []
+  CSV_FILE_NAME = 'contacts.csv'
   attr_reader :name, :email, :id
-
+  
+  # Creates a new contact object
+  # @param name [String] The contact's name
+  # @param email [String] The contact's email address
   def initialize(name, email)
-    @name = name
-    @email = email
+    @name = name.strip
+    @email = email.strip
     @id = @@contacts_count
-    @@contacts_count+=1
+    @@contacts_count += 1
   end
 
   # Returns a formatted String of the contact's name and email
@@ -16,14 +24,56 @@ class Contact
     "#{self.id}:\t#{self.name}\t(#{self.email})"
   end
 
-  # Returns a String with the contact's name and email on separate lines
-  def show_details
-    "#{self.name}\n#{self.email}\n"
+  # Returns true if the contact's name and/or email matches search_term, false otherwise
+  def matches?(term)
+    term = /#{term.downcase}/
+    !(term =~ self.name.downcase).nil? || !(term =~ self.email.downcase).nil?
   end
 
-  # Returns true if the contact's name and/or email matches search_term, false otherwise
-  def matches?(search_term)
-    search_term = /#{search_term.downcase}/
-    !(search_term =~ self.name.downcase).nil? || !(search_term =~ self.email.downcase).nil?
+  # Provides functionality for managing contacts in the csv file.
+  class << self
+
+    # Opens 'contacts.csv' and creates a Contact object for each line in the file (aka each contact).
+    # @return [Array<Contact>] Array of Contact objects
+    def all
+      return unless File.file?(CSV_FILE_NAME)
+      @@contacts = CSV.read(CSV_FILE_NAME)
+      @@contacts.map! { |contact| Contact.new(contact[1], contact[2]) }
+    end
+
+    # Creates a new contact, adding it to the csv file, returning the new contact.
+    # @param name [String] the new contact's name
+    # @param email [String] the contact's email
+    def create(name, email)
+      contact = Contact.new(name, email)
+      @@contacts << contact
+      update_csv_file
+      contact
+    end
+    
+    # Find the Contact in the 'contacts.csv' file with the matching id.
+    # @param id [Integer] the contact id
+    # @return [Contact, nil] the contact with the specified id. If no contact has the id, returns nil.
+    def find(id)
+     @@contacts.detect { |contact| contact.id == id }
+    end
+    
+    # Search for contacts by either name or email.
+    # @param term [String] the name fragment or email fragment to search for
+    # @return [Array<Contact>] Array of Contact objects.
+    def search(term)
+      @@contacts.select { |contact| contact.matches?(term) }
+    end
+
+    private
+
+    # Update the contacts.csv file
+    def update_csv_file
+      csv_file = CSV.open(CSV_FILE_NAME, "w")
+      @@contacts.each { |contact| csv_file << [contact.id, contact.name, contact.email] }
+      csv_file.close
+    end
+
   end
+
 end
--- contact_list.rb
+++ contact_list.rb
@@ -1,97 +1,46 @@
-require 'csv'
 require_relative 'contact'
-# Provides the Contact List Application
+
+# Interfaces between a user and their contact list. Reads from and writes to standard I/O.
 class ContactList
 
-  @@contacts = []
-  CSV_FILE_NAME = "contacts.csv"
-
-  class ContactListError < StandardError
-  end
-
-  class << self
-    # Show the application commands
-    def show_commands
-      puts ("*" * 10) + "  COMMANDS  " + ("*" * 10)
-      puts "new     - Create a new contact"
-      puts "list    - List all contacts"
-      puts "show    - Show a contact"
-      puts "search  - Search for a contact"
-      puts "*" * 32
-    end
-
-    # Run the application
-    def run
-      retrieve_current_contacts
-      show_commands if ARGV.length == 0
-      case ARGV[0]
-      when "list"
-        list_contacts
-      when "new"
-        contact = create_contact
-        @@contacts << contact
-        update_csv_file
-        puts "Added #{contact.name} with ID #{contact.id} successfully!"
-      when "show"
-        return if ARGV[1].nil?
-        show_contact(ARGV[1].to_i)
-      when "search"
-        return if ARGV[1].nil?
-        found_contacts = find_contacts(ARGV[1])
-        puts (found_contacts.empty?) ? "Could not find contacts that matched #{ARGV[1]}" : found_contacts
-      end
-      rescue ContactListError => error
-        puts "Error when trying to list all contacts: #{error}"
-    end
-
-    # Opens the CSV file, converts its entries into Contacts, and stores them in @@contacts
-    def retrieve_current_contacts
-      return unless File.file?(CSV_FILE_NAME)
-      contacts = CSV.read(CSV_FILE_NAME)
-      contacts.each_with_index do |contact, index|
-        @@contacts << Contact.new(contact[1], contact[2])
-      end
-    end
-
-    # List all contacts from the CSV file
-    def list_contacts
-      puts @@contacts
-      puts "---"
-      puts "#{@@contacts.length} records total"
-    end
-
-    # Return a new contact with the name and email provided by the user
-    def create_contact
-      puts "Enter the new contact's full name: "
+  # Runs the Contact List application
+  def self.run
+    contacts = Contact.all
+    show_options if ARGV.length == 0
+    case ARGV[0]
+    when "list"
+      puts contacts
+    when "new"
+      puts "Enter the new contact's name: "
       name = STDIN.gets.chomp
       puts "Enter the new contact's email: "
       email = STDIN.gets.chomp
-      Contact.new(name,email)
-    end
-
-    # Show the details of the contact with "id"
-    def show_contact(id)
-      contact = find_contact_by_id(id)
-      puts (contact.nil?) ? "Could not find contact with id #{id}" : contact.show_details
-    end
-
-    # Returns the contact with "id" if found, nil otherwise
-    def find_contact_by_id(id)
-      @@contacts.detect { |contact| contact.id == id }
-    end
-
-    # Returns all contacts that match search_term, nil otherwise
-    def find_contacts(search_term)
-      @@contacts.select { |contact| contact.matches?(search_term) }
-    end
-
-    # Store the contacts back into the CSV file
-    def update_csv_file
-      csv_file = CSV.open(CSV_FILE_NAME, "w")
-      @@contacts.each { |contact| csv_file << [contact.id, contact.name, contact.email] }
-      csv_file.close
-    end
+      contact = Contact.create(name, email)
+      puts "Added #{contact.name} with ID #{contact.id} successfully!"
+    when "show"
+      return if ARGV[1].nil?
+      contact = Contact.find(ARGV[1].to_i)
+      puts (contact.nil?) ? "Contact with ID #{ARGV[1]} does not exist." : contact_details(contact)
+    when "search"
+      return if ARGV[1].nil?
+      found_contacts = Contact.search(ARGV[1])
+      puts (found_contacts.empty?) ? "Did not find any contacts that matched #{ARGV[1]}" : found_contacts
+    end 
   end
-end
 
-ContactList.run
+  # Show the options for the Contact List app
+  def self.show_options
+    puts ("*" * 10) + "  COMMANDS  " + ("*" * 10)
+    puts "new     - Create a new contact"
+    puts "list    - List all contacts"
+    puts "show    - Show a contact"
+    puts "search  - Search for a contact"
+    puts "*" * 32
+  end
+
+  # Return a String with the contact's name and email on separate lines
+  def self.contact_details(contact)
+    "#{contact.name}\n#{contact.email}"
+  end
+
+end
